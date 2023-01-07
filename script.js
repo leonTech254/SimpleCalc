46d8ab7ec6d43810e46d44cccb2b9cd2407f27ef
leontech <martinleontech23@gmail.com>
2023-01-07 13:37:39 +0300

    ---update

---
+++ script.js
@@ -0,0 +1,21 @@
+function myfun()
+{
+    let x=document.getElementById("inputs").value;
+    let result=eval(x);
+    document.getElementById("results").value=result; 
+}
+
+function power()
+{
+    
+
+    let power=document.getElementById("inputs").value="Thank you";
+    document.getElementById("results").value="leonTech-191:OFF";
+    Refresh(999);
+}
+function Clear()
+{
+    
+    document.getElementById("results").value="";  
+    document.getElementById("inputs").value="";
+}
